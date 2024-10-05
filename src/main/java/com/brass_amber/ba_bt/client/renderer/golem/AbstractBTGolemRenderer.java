bcfbbf4481aba805f3cbd76f5434b7a0da1247dd
ryan-craven <benjamin.r.craven@gmail.com>
2024-10-05 19:37:55 -0400

    Quick fix to render nether golem damage layers.

--- src/main/java/com/brass_amber/ba_bt/client/renderer/golem/AbstractBTGolemRenderer.java
+++ src/main/java/com/brass_amber/ba_bt/client/renderer/golem/AbstractBTGolemRenderer.java
@@ -3,6 +3,7 @@ package com.brass_amber.ba_bt.client.renderer.golem;
 import com.brass_amber.ba_bt.BABTMain;
 import com.brass_amber.ba_bt.entity.hostile.golem.BTAbstractGolem;
 
+import com.brass_amber.ba_bt.entity.hostile.golem.BTNetherGolem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -31,6 +32,25 @@ public abstract class AbstractBTGolemRenderer<E extends BTAbstractGolem, M exten
 
 	@Override
 	public ResourceLocation getTextureLocation(BTAbstractGolem entity) {
+
+		if (entity instanceof BTNetherGolem netherGolem) {
+			float missingHealthPercentage = (netherGolem.getMaxHealth() - netherGolem.getHealth()) / netherGolem.getMaxHealth();
+			BABTMain.LOGGER.info("NETHER GOLEM HEALTH PERCENTAGE REMAINING: {}", missingHealthPercentage);
+
+			if (missingHealthPercentage < 0.25) {
+				return entity.isAwake() ? golemTexturesAwaken : golemTexturesDormant;
+			}
+			else if (missingHealthPercentage >= 0.25 && missingHealthPercentage < 0.5) {
+				return BABTMain.locate("textures/entity/golem/" + this.golemType + "/nether_golem_cracked_stage_1.png");
+			}
+			else if (missingHealthPercentage >= 0.5 && missingHealthPercentage < 0.75) {
+				return BABTMain.locate("textures/entity/golem/" + this.golemType + "/nether_golem_cracked_stage_2.png");
+			}
+			else {
+				return BABTMain.locate("textures/entity/golem/" + this.golemType + "/blaze_metal_golem/blaze_metal_golem.png");
+			}
+		}
+
 		return entity.isEnraged() ? golemTexturesEnraged : entity.isAwake() ? golemTexturesAwaken : golemTexturesDormant;
 	}
 
